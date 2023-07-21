901b120f883b8a9947a4c23646cb194055544114
Yash Aggarwal <yashaggarwal0009@gmail.com>
2023-07-21 22:45:51 +0530

    second update

--- GITTYJAVA.java
+++ GITTYJAVA.java
@@ -4,7 +4,7 @@ class GITTYJAVA{
     public static void main(String args[]){
     int a = 5;
     int b = 6;
-    System.out.println(a+b);
+    System.out.println("sum is this bruh: "a+b);
     }
 }
 
